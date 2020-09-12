b7248ec5be80c63d5624f796a1cf9afb153d8791
Jason Ozulumba <cjozulumba@gmail.com>
2020-09-11 20:30:44 -0400

    Past, Present, and future classes added and called upon correctly

--- script.js
+++ script.js
@@ -5,28 +5,57 @@ $(document).ready(function () {
   var today = moment().format("MMMM, Do YYYY");
   currentDay.text(today);
 
-  var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  var workHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-  // Need to use work
-  workHours.forEach(function (workHours) {
+// Need to use work
+  workHours.forEach(function (hours) {
+      console.log(hours);
     var row = $("<div>");
     var hourBlock = $("<div>");
     var todoBlock = $("<textarea>");
     var saveBtn = $("<button>");
-    var slotHour = "";
+    var slotHour
 
+ // Generating items "hard coded" 
     row.addClass("row");
-    hourBlock.text("9PM");
+//======= This needs to be ahead so hourBlock can read the value correctly=============
+    if (hours <= 11) {
+        slotHour = hours + "AM";
+    } else if (hours == 12) {
+        slotHour = hours + "PM";
+    } else {
+        slotHour = (hours - 12) + "PM";
+    }
+//=====================================================================================
+    hourBlock.text(slotHour);
     hourBlock.addClass("col-2 time-block hour");
     todoBlock.text("Enter what you plan to do here");
     todoBlock.addClass("col-9 row text-column description");
     saveBtn.attr("type", "submit");
-    saveBtn.attr("value", workHours);
+    saveBtn.attr("value", hours);
     saveBtn.addClass("saveBtn col-1");
+//======== End of "hard coded" items ==================================================
 
+
+
+//======== Start of Conditionals and Functions to be called on ========================
+    if (hours < moment().hour()) {
+        todoBlock.addClass("past");
+    } else if (hours == moment().hour()) {
+        todoBlock.addClass("present");
+    } else {
+        todoBlock.addClass("future");
+    }
+
+
+
+//======= Appending Items to the div with the container class =========================
     calender.append(row);
     row.append(hourBlock);
     row.append(todoBlock);
     row.append(saveBtn);
+
+
+    
   });
 });
